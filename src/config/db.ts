03e3ef1513d44534a476b1ef83dfa5e0bc12eff0
MiltonBlack <arziblack2@gmail.com>
2024-09-15 19:35:08 +0100

    feat: added the prod uri for mongodb...

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
-import { MONGO_URI } from "../constants/conn";
+import { MONGO_PROD_URI, MONGO_URI } from "../constants/conn";
 import { bgMagenta } from "colors";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URI);
+    await mongoose.connect(MONGO_PROD_URI);
     console.log(bgMagenta(`MongoDB Server up and running at ${MONGO_URI}`));
+    console.log(bgMagenta(`MongoDB Server up and running at ${MONGO_PROD_URI}`));
   } catch (error) {
     console.log(error);
     process.exit(1);
