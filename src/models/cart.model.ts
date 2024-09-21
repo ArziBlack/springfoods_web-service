c18e02ab184ad7bdd044bd25219aa982e671716e
MiltonBlack <arziblack2@gmail.com>
2024-09-21 14:36:18 +0100

    feat: fixed cart model and controller...

--- src/models/cart.model.ts
+++ src/models/cart.model.ts
@@ -1,11 +1,10 @@
 import { Schema, model, Types } from "mongoose";
-import { CartItem } from "./cart_item.model";
 import { ICart } from "../interfaces/cart";
+import { cartItemSchema } from "./cart_item.model";
 
-// fix cart items later, for now i just want to let the code compile...
 const cartSchema = new Schema({
   user_id: { type: Types.ObjectId, ref: "User", required: true },
-  items: [{ type: Types.ObjectId, ref: "user" }],
+  items: {type : [cartItemSchema]},
   price: { type: Number, required: true },
   total: { type: Number, required: true },
 });
--- src/models/cart_item.model.ts
+++ src/models/cart_item.model.ts
@@ -1,10 +1,10 @@
 import { Schema, model, Types } from "mongoose";
 
-const cartItemSchema = new Schema({
+export const cartItemSchema = new Schema({
   cart_id: { type: String },
   product: { type: Types.ObjectId, ref: "Product", required: true },
   quantity: { type: Number, required: true, default: 1 },
   price: { type: Number, required: true },
-});
+}, { _id: true });
 
 export const CartItem = model("CartItem", cartItemSchema);
