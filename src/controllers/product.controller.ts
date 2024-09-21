8612aa966ad571271c44984d51a73da094dc7a43
MiltonBlack <arziblack2@gmail.com>
2024-09-21 22:20:23 +0100

    fix: object id cast...

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -359,10 +359,13 @@ export const get_all_products_with_reviews = async (req: TypedRequest,
 }
 
 // GET ALL FEATURED PRODUCTS
-export const get_featured_products = async (req: TypedRequest, res: Response, next: NextFunction) => {
+export const get_featured_products = async (req:TypedRequest, res:Response, next: NextFunction ) => {
   try {
+    const page = parseInt(req.query.page as string) || 1;
+    const limit = parseInt(req.query.limit as string) || 10;
+    const skip = (page - 1) * limit;
 
-    const products = await Product.find();
+    const products = await Product.find({ featured: true }).skip(skip).limit(limit);
 
     if (!products.length) {
       return res.status(200).json({
@@ -372,18 +375,25 @@ export const get_featured_products = async (req: TypedRequest, res: Response, ne
       });
     }
 
-    const totalProducts = products.filter(item => item.featured === true);
+    const no_of_products = products.length;
 
-    const no_of_products = totalProducts.length;
+    const pagination = {
+      totalItems: no_of_products,
+      totalPages: Math.ceil(no_of_products / limit),
+      currentPage: page,
+      hasNextPage: page < Math.ceil(no_of_products / limit),
+      hasPrevPage: page > 1,
+    };
 
-    const product_response: IProductResponse[] = products.map(
-      mapProductDocumentToResponse
-    );
+    // const product_response: IProductResponse[] = products.map(
+    //   mapProductDocumentToResponse
+    // );
 
-    const successResponse: ApiResponse<IProductResponse[]> = {
+    const successResponse: ApiResponse<IProductResponse[] | any> = {
       success: true,
       message: "Products fetched successfully👍",
-      data: product_response,
+      data: products,
+      pagination: pagination,
     };
 
     res.status(200).json(successResponse);
