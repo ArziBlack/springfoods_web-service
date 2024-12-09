fe6857e45fa8538dc491b4200b4270e80d958b37
Milton Black <arziblack2@gmail.com>
2024-12-09 08:27:37 +0100

    feat: added delete review from a product route

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   add_review_to_a_product,
+  delete_review_from_a_product,
   get_all_products,
   get_all_products_by_category,
   get_all_products_by_reviews,
@@ -10,7 +11,11 @@ import {
 } from "../controllers/product.controller";
 import { verify } from "../services/verifyService";
 import { validateParams } from "../middleware/validate";
-import { idSchema, productIdSchema, productReviewSchema } from "../services/validation/productValidator";
+import {
+  idSchema,
+  productIdSchema,
+  productReviewSchema,
+} from "../services/validation/productValidator";
 
 const router = Router();
 
@@ -18,16 +23,31 @@ const router = Router();
 router.route("/").get(get_all_products);
 
 // GET PRODUCTS BY CATEGORY ID
-router.route("/category/:id").get(verify, validateParams(idSchema), get_all_products_by_category);
+router
+  .route("/category/:id")
+  .get(verify, validateParams(idSchema), get_all_products_by_category);
 
 // GET PRODUCTS BY REVIEWS
 router.route("/reviews").get(verify, get_all_products_by_reviews);
 
 // GET PRODUCTS WITH REVIEWS
-router.route("/reviews/:product_id").get(verify, validateParams(productIdSchema), get_all_products_with_reviews);
+router
+  .route("/reviews/:product_id")
+  .get(verify, validateParams(productIdSchema), get_all_products_with_reviews);
 
 // ADD REVIEW TO A PRODUCT
-router.route("/review/:user_id/:product_id").post(verify, validateParams(productReviewSchema), add_review_to_a_product);
+router
+  .route("/review/:user_id/:product_id")
+  .post(verify, validateParams(productReviewSchema), add_review_to_a_product);
+
+// REMOVE REVIEW FROM A PRODUCT
+router
+  .route("/delete-review/:user_id/:product_id")
+  .post(
+    verify,
+    validateParams(productReviewSchema),
+    delete_review_from_a_product
+  );
 
 // GET PRODUCT BY ID (SINGLE PRODUCT)
 router.route("/:id").get(verify, validateParams(idSchema), get_product_by_ID);
